41e48cc9d865c6aa2343142bf165d4a678a3f157
Qurin3 <124502189+Qurin3@users.noreply.github.com>
2023-12-31 11:14:56 +0100

    sidebar finished

--- portfolio/src/components/navbar/sidebar/links/Links.jsx
+++ portfolio/src/components/navbar/sidebar/links/Links.jsx
@@ -1,43 +1,45 @@
-import React from 'react'
-import  {motion} from "framer-motion"
-const variants = { 
-  open:{
+import React from "react";
+import { motion } from "framer-motion";
+const variants = {
+  open: {
     transition: {
-      staggeredChildren: 0.1
-    }
-  }, 
-  closed:{
+      staggerChildren: 0.2,
+    },
+  },
+  closed: {
     transition: {
-      staggeredChildren: 0.05,
-      staggeredDirection: -1
-    }
-  } 
-}
-const itemVariants = { 
-  open:{
-    transition: {
-      staggeredChildren: 0.1
-    }
-  }, 
-  closed:{
-    transition: {
-      staggeredChildren: 0.05,
-      staggeredDirection: -1
-    }
-  } 
-}
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
 const Links = () => {
-  const items = [
-    "Home",
-    "Portfolio",
-    "Contact",
-    "About"
-  ]
+  const items = ["Home", "Portfolio", "Contact", "About"];
   return (
-    <motion.div className='links' variants={variants}>
-      {items.map((item)=>(<a href={`#${item}`} key={item}>{item}</a>))}
+    <motion.div className="links" variants={variants}>
+      {items.map((item) => (
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.15 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          {item}
+        </motion.a>
+      ))}
     </motion.div>
-  )
-}
+  );
+};
 
-export default Links
+export default Links;
