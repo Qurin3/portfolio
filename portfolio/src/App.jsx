ed55cef1178827bd60253bc5316dafa4b32657e4
Qurin3 <124502189+Qurin3@users.noreply.github.com>
2023-12-31 12:44:04 +0100

    moving bar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import "./app.scss"
+import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 const App = () => {
   return (
     <div>
       <section id="Home">
         <Navbar />
+        <Hero />
       </section>
       <section>Parallax</section>
       <section id="Portfolio">Portfolio</section>
