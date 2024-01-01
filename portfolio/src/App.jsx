49074d43d86af2a97be03b51d2bae20f4e2b1220
Qurin3 <124502189+Qurin3@users.noreply.github.com>
2024-01-01 21:10:02 +0100

    creating portfolio section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss"
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import Parallax from "./components/parallax/Parallax"
+import Portfolio from "./components/portfolio/Portfolio"
 import Services from "./components/services/Services"
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <section id="Services"><Parallax type="services" /></section>
       <section><Services /></section>
       <section id="Portfolio"><Parallax type="portfolio" /></section>
+      <Portfolio />
       <section id="Contact">Contact me</section>
       <section id="About">About</section>
     </div>
---
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./portfolio.scss"
+const Portfolio = () => {
+  return (
+    <div className='portfolio'>portfolio</div>
+  )
+}
+
+export default Portfolio
