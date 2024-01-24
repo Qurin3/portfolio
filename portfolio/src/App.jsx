fb4318c6bb8d26cd2043fe782d327580b1977bff
Qurin3 <124502189+Qurin3@users.noreply.github.com>
2024-01-24 11:44:43 +0100

    poprawki

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,7 +2,6 @@ import "./app.scss"
 import Contact from "./components/contact/Contact"
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
-import Parallax from "./components/parallax/Parallax"
 import Portfolio from "./components/portfolio/Portfolio"
 import Services from "./components/services/Services"
 const App = () => {
@@ -12,9 +11,8 @@ const App = () => {
         <Navbar />
          <Hero /> 
       </section>
-      <section id="Services"><Parallax type="services" /></section>
       <section><Services /></section> 
-      <section id="Portfolio"><Parallax type="portfolio" /></section> 
+
        <Portfolio />
        <section id="Contact">
         <Contact />
--- portfolio/src/components/services/Services.jsx
+++ portfolio/src/components/services/Services.jsx
@@ -1,62 +1,13 @@
 import React from "react";
 import "./services.scss";
-import { motion,useInView } from "framer-motion";
-import { useRef } from "react";
-const variants = {
-    initial: {
-        x: -80,
-        opacity: 0
-    },
-    animate: {
-        x: 0,
-        opacity: 1,
-        transition: {
-            duration: 1,
-            staggerChildren: 0.1
-        }
-    },
-};
 const Services = () => {
-    const ref = useRef()
-    const isInView = useInView(ref,{margin:"-100px"})
   return (
     
-    <motion.div className="services" variants={variants} initial="initial" ref={ref} animate={isInView && 'animate'}>
-      <motion.div className="textContainer" variants={variants}>
-        <p>
-          I am searching for a first internship
-          <br /> to rocket my knowledge!
-        </p>
-        <hr />
-      </motion.div>
-      <motion.div className="titleContainer" variants={variants}>
-        <div className="title">
-            <img src="/people.webp" alt="people" />
-            <h1><motion.b whileHover={{color:"orange"}}>Unique</motion.b> Ideas</h1>
-        </div>
-        <div className="title">
-            <h1><motion.b whileHover={{color:"orange"}}>For your</motion.b> Business</h1>
-            <button>What we do?</button>
-        </div>
-      </motion.div>
-      <motion.div className="listContainer" variants={variants}>
-            <motion.div className="box-idea" whileHover={{background: "lightgray",color:"black"}}>
-                <h2>Branding</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                <button>GO</button>
-            </motion.div>
-            <motion.div className="box-idea" whileHover={{background: "lightgray",color:"black"}}>
-                <h2>Branding</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                <button>GO</button>
-            </motion.div>
-            <motion.div className="box-idea" whileHover={{background: "lightgray",color:"black"}}>
-                <h2>Branding</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                <button>GO</button>
-            </motion.div>
-      </motion.div>
-    </motion.div>
+    <div className="services">
+      <div className="wrapper"> 
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e7008a" fill-opacity="1" d="M0,288L48,256C96,224,192,160,288,165.3C384,171,480,245,576,261.3C672,277,768,235,864,218.7C960,203,1056,213,1152,234.7C1248,256,1344,288,1392,304L1440,320L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+    </div>
+    </div>
   );
 };
 
