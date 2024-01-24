b7e1a107ebe03e1e7be2cec0da5b6f56a6ef3bba
Qurin3 <124502189+Qurin3@users.noreply.github.com>
2024-01-24 21:48:01 +0100

    small changes

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import './about.scss'
-import ImageLoli from '../../../public/loli.png'
+import React from 'react';
+import './about.scss';
+import ImageLoli from '../../../public/loli.png';
+
 const About = () => {
   return (
     <>
-   
-    <div className='wrap'>
-      <div className="imageCon">
-        <img src={ImageLoli} alt="lolipop" height="600px"/>
-      </div>
-        <div className="text">
-        <h1>About me</h1>
-        I am an enthusiastic front-end developer with a passion for creating compelling web experiences. Proficient in React, Next.js, Tailwind CSS and TypeScript, I combine creativity with technology to bring ideas to life. Currently studying Computer Science, I am also interested in backend development. With previous corporate experience, I bring technical skills and professionalism to my work. Let's create something extraordinary! 
-        <br /><button className='button1'>Check out my CV!</button>
+      <div className='wrap'>
+        <div className="imageCon">
+          <img src={ImageLoli} alt="lolipop" className="loliImage" />
         </div>
-    </div>
+        <div className="text">
+          <h1>About me</h1>
+          <p>
+            I am an enthusiastic front-end developer with a passion for creating compelling web experiences. Proficient in React, Next.js, Tailwind CSS, and TypeScript, I combine creativity with technology to bring ideas to life. Currently studying Computer Science, I am also interested in backend development. With previous corporate experience, I bring technical skills and professionalism to my work. Let's create something extraordinary!
+          </p>
+          <button className='button1'>Check out my CV!</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
