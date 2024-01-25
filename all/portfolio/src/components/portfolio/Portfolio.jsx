e978aa51a170cd5b51d8a268ac58839fd84d3fe7
Qurin3 <124502189+Qurin3@users.noreply.github.com>
2024-01-25 14:00:04 +0100

    fix1

--- all/portfolio/src/components/portfolio/Portfolio.jsx
+++ all/portfolio/src/components/portfolio/Portfolio.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { useRef } from "react";
-
+import King from "../../../public/King.jpg"
   const items = [
     {
       id: 1,
       title: "NextJS Car rental",
-      img: "../../public/rental1.jpg",
+      img: "../../..public/rental1.jpg",
       desc: "For this project I used TypeScript, Next and Tailwind. My goal was to learn how to use the API, so I used Cars by API Ninjas."
     },
     {
       id: 2,
       title: "KingoPedia",
-      img: "../../public/King.jpg",
+      img: "../../..public/King.jpg",
       desc: "I noticed that there is no good API on the web that contains all the works of Stephen King. Being a big fan, I couldn't let this pass and decided to create my own API and a page to browse the books of the master of horror. The application is still in production, but it will appear here soon :)",
     },
   ];
