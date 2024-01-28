a35cdd155c2639011158757df09fbc7a902816c2
Qurin3 <124502189+Qurin3@users.noreply.github.com>
2024-01-28 10:54:04 +0100

    fix3 ios

--- all/portfolio/src/components/contact/Contact.jsx
+++ all/portfolio/src/components/contact/Contact.jsx
@@ -1,10 +1,10 @@
-import React, { useRef,useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./contact.scss"
-import {motion, useInView} from 'framer-motion'
+import { motion, useInView } from 'framer-motion'
 import emailjs from '@emailjs/browser'
 
 const Variants = {
-   initial: {
+  initial: {
     x: -80,
     opacity: 0,
   },
@@ -16,56 +16,56 @@ const Variants = {
       staggerChildren: 0.1,
     },
   },
-  };
-  
-    
+};
+
+
 const Contact = () => {
-    const formRef = useRef()
-    const [error, setError] = useState(false)
-    const [success, setSuccess] = useState(false)
-    const sendEmail = (e) => {
-        e.preventDefault();
-    
-        emailjs.sendForm('service_udjvi98', 'template_8p9v81s', formRef.current, 'uZP_qItRU8jcZetM7')
-          .then((result) => {
-            setSuccess(true)
-          }, (error) => {
-            setError(true)
-          });
-      };
-      const ref = useRef()
-      const isInView = useInView(ref,{margin:"-100px"})
-    
+  const formRef = useRef()
+  const [error, setError] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_udjvi98', 'template_8p9v81s', formRef.current, 'uZP_qItRU8jcZetM7')
+      .then((result) => {
+        setSuccess(true)
+      }, (error) => {
+        setError(true)
+      });
+  };
+  const ref = useRef()
+  const isInView = useInView(ref, { margin: "-100px" })
+
   return (
     <motion.div className='contact' variants={Variants} initial="initial" ref={ref} animate={isInView && 'animate'}>
-        <motion.div className="textContainer" variants={Variants}>
-            <motion.h1 variants={Variants}>Lets work together!</motion.h1>
-            <motion.div className="item" variants={Variants}>
-                <h2>Mail</h2>
-                <span>filip.klv2@gmail.com</span>
-            </motion.div>
-            <motion.div className="item" variants={Variants} >
-                <h2>Phone</h2>
-                <span>509 794 050</span>
-            </motion.div>
+      <motion.div className="textContainer" variants={Variants}>
+        <motion.h1 variants={Variants}>Lets work together!</motion.h1>
+        <motion.div className="item" variants={Variants}>
+          <h2>Mail</h2>
+          <span>filip.klv2@gmail.com</span>
         </motion.div>
-        <div className="formContainer">
-            <motion.form
-            ref={formRef}
-            onSubmit={sendEmail}
-            variants={Variants}
-            initial="initial"
-            
-            animate={isInView && 'animate'}
-    >
-                <input type="text" placeholder='Name' required name='name' />
-                <input type="email" placeholder='Email' required name='email' />
-                <textarea rows={8} placeholder='Message' name='message'/>
-                <button>Submit</button>
-                {error && "failure"}
-                {success && "success"}
-            </motion.form>
-        </div>
+        <motion.div className="item" variants={Variants} >
+          <h2>Phone</h2>
+          <span>+48 509 794 050</span>
+        </motion.div>
+      </motion.div>
+      <div className="formContainer">
+        <motion.form
+          ref={formRef}
+          onSubmit={sendEmail}
+          variants={Variants}
+          initial="initial"
+
+          animate={isInView && 'animate'}
+        >
+          <input type="text" placeholder='Name' required name='name' />
+          <input type="email" placeholder='Email' required name='email' />
+          <textarea rows={8} placeholder='Message' name='message' />
+          <button>Submit</button>
+          {error && "failure"}
+          {success && "success"}
+        </motion.form>
+      </div>
     </motion.div>
   )
 }
--- all/portfolio/src/components/hero/Hero.jsx
+++ all/portfolio/src/components/hero/Hero.jsx
@@ -20,14 +20,14 @@ const textVariants = {
     opacity: 0,
     y: 10,
     transition: {
-        duration: 2,
-        repeat: Infinity
+      duration: 2,
+      repeat: Infinity
     }
   },
 };
 const Hero = () => {
   return (
-    
+
     <div className="hero">
       <div className="wrapper">
         <motion.div
@@ -43,19 +43,18 @@ const Hero = () => {
               Scroll down to see the Latest work or Contact me :D
             </motion.button>
           </motion.div>
-          <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="" />
         </motion.div>
         <motion.div className="imageContainer" variants={textVariants}>
-         {/*   <Blob />   */} 
-         <motion.img src={Image} alt="profile" variants={textVariants} initial="initial"
-          animate="animate"/>
+          {/*   <Blob />   */}
+          <motion.img src={Image} alt="profile" variants={textVariants} initial="initial"
+            animate="animate" />
         </motion.div>
-        
+
       </div>
       <div className="slidingTextContainer">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e7008a" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,234.7C384,224,480,192,576,181.3C672,171,768,181,864,202.7C960,224,1056,256,1152,234.7C1248,213,1344,139,1392,101.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e7008a" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,234.7C384,224,480,192,576,181.3C672,171,768,181,864,202.7C960,224,1056,256,1152,234.7C1248,213,1344,139,1392,101.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
       </div>
-      
+
     </div>
   );
 };
