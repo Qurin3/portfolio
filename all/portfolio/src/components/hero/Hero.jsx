80319aa9c0e92a8e745a162d042041a7fdba4b86
Qurin3 <124502189+Qurin3@users.noreply.github.com>
2024-02-02 23:49:00 +0100

    poprawki

--- all/portfolio/src/components/hero/Hero.jsx
+++ all/portfolio/src/components/hero/Hero.jsx
@@ -1,9 +1,6 @@
-import React from "react";
 import "./hero.scss";
 import { motion } from "framer-motion";
-import Blob from './Blob'
 import loli from "../../../public/loli.png"
-import Image from "../../../public/profile2.png"
 const textVariants = {
   initial: {
     x: -500,
@@ -53,7 +50,7 @@ const Hero = () => {
 
       </div>
       <div className="slidingTextContainer">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e7008a" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,234.7C384,224,480,192,576,181.3C672,171,768,181,864,202.7C960,224,1056,256,1152,234.7C1248,213,1344,139,1392,101.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e7008a" d="M0,224L48,229.3C96,235,192,245,288,234.7C384,224,480,192,576,181.3C672,171,768,181,864,202.7C960,224,1056,256,1152,234.7C1248,213,1344,139,1392,101.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
       </div>
 
     </div>
